8a62d52ab3a2b974ca75a576031b21f86e57bcc3
Rich Garner <richgarner71@gmail.com>
2022-08-31 12:48:08 -0400

    Upload current gulp

--- gulpfile-uswds.js
+++ gulpfile-uswds.js
@@ -1,189 +1,42 @@
-/*
-* * * * * ==============================
-* * * * * ==============================
-* * * * * ==============================
-* * * * * ==============================
-========================================
-========================================
-========================================
-----------------------------------------
-USWDS SASS GULPFILE
-----------------------------------------
-*/
+/* gulpfile.js */
 
-const autoprefixer = require("autoprefixer");
-const csso = require("postcss-csso");
-const gulp = require("gulp");
-const pkg = require("./node_modules/@uswds/uswds/package.json");
-const postcss = require("gulp-postcss");
-const replace = require("gulp-replace");
-const sass = require("gulp-sass")(require("sass"));
-const sourcemaps = require("gulp-sourcemaps");
-const uswds = require("./node_modules/uswds-gulp/config/uswds");
-const del = require("del");
-const svgSprite = require("gulp-svg-sprite");
-const rename = require("gulp-rename");
+const uswds = require("@uswds/compile");
 
-/*
-----------------------------------------
-PATHS
-----------------------------------------
-- All paths are relative to the
-  project root
-- Don't use a trailing `/` for path
-  names
-----------------------------------------
-*/
+/**
+ * USWDS version
+ */
 
-// Project Sass source directory
-const PROJECT_SASS_SRC = "./assets/sass";
+uswds.settings.version = 3;
 
-// Images destination
-const IMG_DEST = "./assets/images";
+/**
+ * Path settings
+ * Set as many as you need
+ */
 
-// Fonts destination
-const FONTS_DEST = "./assets/fonts";
+uswds.paths.dist.css = './_assets/css';
+uswds.paths.dist.img = './_assets/img';
+uswds.paths.dist.theme = './sass';
 
-// Javascript destination
-const JS_DEST = "./assets/js";
+uswds.paths.src.uswds	= "./node_modules/@uswds";
+uswds.paths.src.sass	= "./node_modules/@uswds/uswds/packages";
+uswds.paths.src.theme	= "./node_modules/@uswds/uswds/dist/theme";
+uswds.paths.src.fonts	= "./node_modules/@uswds/uswds/dist/fonts";	
+uswds.paths.src.img   = "./node_modules/@uswds/uswds/dist/img";	
+uswds.paths.src.js    = "./node_modules/@uswds/uswds/dist/js";
+uswds.paths.src.projectSass = "./sass";
+uswds.paths.src.projectIcons = "";
+uswds.paths.dist.theme = "./sass";
+uswds.paths.dist.img = "./_assets/img";
+uswds.paths.dist.fonts = "./_assets/fonts";
+uswds.paths.dist.js = "./_assets/js";
+uswds.paths.dist.css = "./_assets/css";
+uswds.sprite.projectIconsOnly = false;	
 
-// Compiled CSS destination
-const CSS_DEST = "./assets/css";
 
-// Site CSS destination
-// Like the _site/assets/css directory in Jekyll, if necessary.
-// If using, uncomment line 106
-const SITE_CSS_DEST = "./_site/assets/css";
+/**
+ * Exports
+ * Add as many as you need
+ */
 
-/*
-----------------------------------------
-TASKS
-----------------------------------------
-*/
-
-gulp.task("copy-uswds-setup", () => {
-  return gulp
-    .src(`${uswds}/dist/theme/**/**`)
-    .pipe(gulp.dest(`${PROJECT_SASS_SRC}`));
-});
-
-gulp.task("copy-uswds-fonts", () => {
-  return gulp.src(`${uswds}/dist/fonts/**/**`).pipe(gulp.dest(`${FONTS_DEST}`));
-});
-
-gulp.task("copy-uswds-images", () => {
-  return gulp.src(`${uswds}/dist/img/**/**`).pipe(gulp.dest(`${IMG_DEST}`));
-});
-
-gulp.task("copy-uswds-js", () => {
-  return gulp.src(`${uswds}/dist/js/**/**`).pipe(gulp.dest(`${JS_DEST}`));
-});
-
-gulp.task("build-sass", function (done) {
-  var plugins = [
-    // Autoprefix
-    autoprefixer({
-      cascade: false,
-      grid: true,
-    }),
-    // Minify
-    csso({ forceMediaMerge: false }),
-  ];
-  return (
-    gulp
-      .src([`${PROJECT_SASS_SRC}/*.scss`])
-      .pipe(sourcemaps.init({ largeFile: true }))
-      .pipe(
-        sass({
-          includePaths: [
-            `${PROJECT_SASS_SRC}`,
-            `${uswds}`,
-            `${uswds}/node_modules/@uswds/uswds/packages`,
-          ],
-        })
-      )
-      .pipe(replace(/\buswds @version\b/g, "based on uswds v" + pkg.version))
-      .pipe(postcss(plugins))
-      .pipe(sourcemaps.write("."))
-      // uncomment the next line if necessary for Jekyll to build properly
-      //.pipe(gulp.dest(`${SITE_CSS_DEST}`))
-      .pipe(gulp.dest(`${CSS_DEST}`))
-  );
-});
-
-// SVG sprite configuration
-config = {
-  shape: {
-    dimension: {
-      // Set maximum dimensions
-      maxWidth: 24,
-      maxHeight: 24,
-    },
-    id: {
-      separator: "-",
-    },
-    spacing: {
-      // Add padding
-      padding: 0,
-    },
-  },
-  mode: {
-    symbol: true, // Activate the «symbol» mode
-  },
-};
-
-gulp.task("build-sprite", function (done) {
-  gulp
-    .src(`${IMG_DEST}/usa-icons/**/*.svg`, {
-      allowEmpty: true,
-    })
-    .pipe(svgSprite(config))
-    .on("error", function (error) {
-      console.log("There was an error");
-    })
-    .pipe(gulp.dest(`${IMG_DEST}`))
-    .on("end", function () {
-      done();
-    });
-});
-
-gulp.task("rename-sprite", function (done) {
-  gulp
-    .src(`${IMG_DEST}/symbol/svg/sprite.symbol.svg`, {
-      allowEmpty: true,
-    })
-    .pipe(rename(`${IMG_DEST}/sprite.svg`))
-    .pipe(gulp.dest(`./`))
-    .on("end", function () {
-      done();
-    });
-});
-
-gulp.task("clean-sprite", function (cb) {
-  cb();
-  return del.sync(`${IMG_DEST}/symbol`);
-});
-
-gulp.task(
-  "init",
-  gulp.series(
-    "copy-uswds-setup",
-    "copy-uswds-fonts",
-    "copy-uswds-images",
-    "copy-uswds-js",
-    "build-sass"
-  )
-);
-
-gulp.task("watch-sass", function () {
-  gulp.watch(`${PROJECT_SASS_SRC}/**/*.scss`, gulp.series("build-sass"));
-});
-
-gulp.task("watch", gulp.series("build-sass", "watch-sass"));
-
-gulp.task("default", gulp.series("watch"));
-
-gulp.task(
-  "svg-sprite",
-  gulp.series("build-sprite", "rename-sprite", "clean-sprite")
-);
+exports.init = uswds.init;
+exports.compile = uswds.compile;
